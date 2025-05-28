0a044e6edf49022d120e96df8114a12d5f07aafe
J. Kirby Ross <1207477+flyingrobots@users.noreply.github.com>
2025-05-28 15:41:57 -0700

    chore(mocks): export mock modules via test barrel index

--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -1,2 +1,5 @@
 export { MockFileCache } from './MockFileCache';
 export { TestDoubleFactory } from './TestDoubleFactory';
+export { MockFileSystem } from './MockFileSystem';
+export { MockLogger } from './MockLogger';
+export { MockClock } from './MockClock';
